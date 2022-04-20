afc13332896cb1ec61044e6ab7772149e9f28a9f
6znj <119874842+6znj@users.noreply.github.com>
2022-04-20 21:00:49 +0000

    Improve API

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,3 +1,8 @@
 import { Module, OnModuleInit } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { Conf
+import { ConfigModule } from './config/config.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { MongoConnectionService } from './shared/mongo-connection.service';
+import { ScriptGeneratorModule } from './script-generator/script-generator.module';
+import { SharedModule } from './shared/sh
