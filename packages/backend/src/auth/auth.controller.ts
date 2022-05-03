261450f986c74aec615c2352fb22b62ea0dd8b3e
6znj <119874842+6znj@users.noreply.github.com>
2022-05-03 17:34:25 +0000

    improve api

--- packages/backend/src/auth/auth.controller.ts
+++ packages/backend/src/auth/auth.controller.ts
@@ -6,4 +6,14 @@ import {
   Req,
   Post,
   Body,
-} from '@n
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ConfigService } from '../config/config.service';
+import { AuthService, Provider } from './auth/auth.service';
+import * as bcrypt from 'bcrypt';
+import { promisify } from 'util';
+import { UserRepository } from './user.repository';
+
+
+/**
+ * Authentication 
