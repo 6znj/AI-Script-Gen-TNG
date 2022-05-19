53da18fc64b8126ba80ad20c6e34e7f54ec002e3
6znj <119874842+6znj@users.noreply.github.com>
2022-05-19 20:08:30 +0000

    removed console log

--- packages/backend/src/script-generator/script-generator.controller.ts
+++ packages/backend/src/script-generator/script-generator.controller.ts
@@ -1,1 +1,9 @@
-import { Controller,
+import { Controller, UseGuards, Get, Post, Body, Param, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ScriptGeneratorService } from './script-generator.service';
+import { ScriptRepository } from './script.repository';
+
+/**
+ * Controller for script generation.
+ */
+@Controller('sc
