7da1fe3fbfbafd7a7cc00458eb308ca1b707040b
6znj <119874842+6znj@users.noreply.github.com>
2022-05-19 18:33:48 +0000

    remove unused modules

--- packages/backend/src/auth/user.repository.ts
+++ packages/backend/src/auth/user.repository.ts
@@ -1,1 +1,10 @@
-import { Injectable } from '@
+import { Injectable } from '@nestjs/common';
+import { BaseMongoRepository } from '../shared/base-mongo.repository';
+
+/**
+ * Repository for Users.
+ */
+@Injectable()
+export class UserRepository extends BaseMongoRepository {
+  public get collection(): any {
+    return this._mongoConnectionService.db.collection('users
