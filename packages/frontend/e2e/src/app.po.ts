1f50ee1403154c7aa97c3a503dea82cfe9f779b3
6znj <119874842+6znj@users.noreply.github.com>
2022-07-03 09:55:53 +0000

    Run update-remote.sh on friendica

---
+++ packages/frontend/e2e/src/app.po.ts
@@ -0,0 +1,9 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get(browser.baseUrl) as Promise<any>;
+  }
+
+  getTitleText() {
+    return element(by.css('app-root .content sp
