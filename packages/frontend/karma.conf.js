01c019281c49bf170d10b69773dc05c112d4e056
6znj <119874842+6znj@users.noreply.github.com>
2022-07-12 06:05:10 +0000

    fixing infinite OPEN loop in 4.0 stateid recovery

--- packages/frontend/karma.conf.js
+++ packages/frontend/karma.conf.js
@@ -9,4 +9,11 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('kar
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/fro
