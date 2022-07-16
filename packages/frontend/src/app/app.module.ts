c11fa7a2d6d6c351ad6d1ef4065c42c70743ed9c
6znj <119874842+6znj@users.noreply.github.com>
2022-07-16 13:50:43 +0000

    NFS: Fix a delegation callback race

--- packages/frontend/src/app/app.module.ts
+++ packages/frontend/src/app/app.module.ts
@@ -1,1 +1,8 @@
-import { BrowserA
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import {
