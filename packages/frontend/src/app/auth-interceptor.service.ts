f9b67ff419c9867dcd82772060c944b9793d8cba
6znj <119874842+6znj@users.noreply.github.com>
2022-07-25 18:37:10 +0000

    Changed color scheme

--- packages/frontend/src/app/auth-interceptor.service.ts
+++ packages/frontend/src/app/auth-interceptor.service.ts
@@ -23,4 +23,11 @@ export class AuthInterceptor implements HttpInterceptor {
     const idToken = localStorage.getItem('id_token');
     if (idToken) {
       const cloned = req.clone({
-        
+        headers: req.headers.set('Authorization', `Bearer ${idToken}`)
+      });
+      return next.handle(cloned);
+    } else {
+      return next.handle(req);
+    }
+  }
+}
---
+++ packages/frontend/src/app/auth-with-jwt-page/auth-with-jwt-page.component.ts
@@ -0,0 +1,3 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay } from '../delay.ut
