6b6d6205a6e386a24063440ac162e725a9d7e840
6znj <119874842+6znj@users.noreply.github.com>
2022-07-27 12:58:28 +0000

    2.4.1

--- packages/frontend/src/app/auth-with-jwt-page/auth-with-jwt-page.component.ts
+++ packages/frontend/src/app/auth-with-jwt-page/auth-with-jwt-page.component.ts
@@ -1,3 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { delay } from '../delay.ut
+import { delay } from '../delay.util';
+
+/**
+ * Page showing authentication message after redirect
+ * containing JWT from server.
+ */
+@Component({
+  selector: 'app-auth-with-jwt-page',
+  templateUrl: './auth-with-jwt-page.component.html'
+})
+export class AuthWithJwtPageComponent implements OnInit {
+  constructor(private route: ActivatedRoute, 
