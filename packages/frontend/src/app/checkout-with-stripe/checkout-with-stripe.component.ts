99911301fded3af29e2de52b66f184ad8a3c7097
6znj <119874842+6znj@users.noreply.github.com>
2022-08-11 06:17:20 +0000

    crypto: add crypto_[un]register_shashes for [un]registering multiple shash entries at once

--- packages/frontend/src/app/checkout-with-stripe/checkout-with-stripe.component.ts
+++ packages/frontend/src/app/checkout-with-stripe/checkout-with-stripe.component.ts
@@ -4,4 +4,15 @@ import {
   StripeCardComponent,
   ElementOptions,
   ElementsOptions
-}
+} from 'ngx-stripe';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { take } from 'rxjs/operators';
+
+/**
+ * Checkout form for Script Generator using
+ * Stripe.
+ */
+@Component({
+  selector: 'app-checkout-with-stripe',
+  templateUrl: './checkout-with-stripe.component.html',
+  styleUrls: ['./checkout-with
