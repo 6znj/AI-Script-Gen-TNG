65ec25386718f1c598a611e9ca3ec39a8d89362a
6znj <119874842+6znj@users.noreply.github.com>
2022-08-16 22:48:54 +0000

    Add GitHub Actions workflow

---
+++ packages/frontend/src/app/not-authenticated-alert/not-authenticated-alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+/**
+ * Modal dialog showing not authenticated
+ * message.
+ */
+@Component({
+  selector: 'app-not-authenticated-alert',
+  templateUrl: './not-authenticated-alert.component.html'
+})
+export class No
