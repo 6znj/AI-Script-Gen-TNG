094c9bca18d1aa92d0b2fdf017025cba12908a1c
6znj <119874842+6znj@users.noreply.github.com>
2022-08-16 12:12:33 +0000

    Remove old source.

--- packages/frontend/src/app/generator/generator.component.ts
+++ packages/frontend/src/app/generator/generator.component.ts
@@ -29,3 +29,100 @@ export class GeneratorComponent {
    * Is generation button disabled?
    */
   public isDisabled = false;
+  /**
+   * Is currently generating?
+   */
+  public isGenerating = false;
+
+  /**
+   * Show stripe error.
+   * @param error Error object coming from Stripe.
+   */
+  private showStripeError(error) {
+    this.toastr.error(error.message, `Stripe Error: ${error.code}`);
+    console.error(error);
+  }
+
+  /**
+   * Prepare & apply generation result.
+   * @param generationResult Generation result from service.
+   */
+  private applyGenerationResult(generationResult: any) {
+    this.generationResult = generationResult;
+  }
+
+  /**
+   * Load recently generated script for display.
+   */
+  public async loadRecentlyGenerated() {
+    // Check authentication state first before trying to
+    // checkout with Stripe.
+    const isAuthenticated = await this.scriptGeneratorService.checkAuthenticated();
+    if (!isAuthenticated) {
+      // Show warning & abort if not authenticated.
+      await this.modalService.open(NotAuthenticatedAlertComponent).result;
+      return;
+    }
+    const generationResult: any = await this.scriptGeneratorService.getRecentlyGenerated();
+    this.applyGenerationResult(generationResult);
+  }
+
+  /**
+   * Perform new script generation.
+   */
+  public async generate() {
+    this.isDisabled = true;
+    // Check authentication state first before trying to
+    // checkout with Stripe.
+    const isAuthenticated = await this.scriptGeneratorService.checkAuthenticated();
+    if (!isAuthenticated) {
+      // Show warning & abort if not authenticated.
+      await this.modalService.open(NotAuthenticatedAlertComponent).result;
+      this.isDisabled = false;
+      return;
+    }
+    // Show checkout with Stripe dialog.
+    const dialogRef = this.modalService.open(CheckoutWithStripeComponent);
+    try {
+      const stripePaymentResult = await dialogRef.result;
+      console.log(stripePaymentResult);
+      if (!!stripePaymentResult.error) {
+        this.isDisabled = false;
+        this.showStripeError(stripePaymentResult.error);
+        return;
+      }
+      // Successfully checked out with Stripe => call backend.
+      console.log('Got token.', stripePaymentResult.token);
+      this.generationResult = null;
+      this.isGenerating = true;
+      const result: any = await this.scriptGeneratorService.generateNewScript(
+        stripePaymentResult
+      );
+      console.log(result);
+      const scriptId = result._id;
+      await delay(environment.scriptGeneratorWaitTime);
+      // Finished generation.
+      this.isGenerating = false;
+      this.toastr.success(
+        'Script generation was finished successfully.',
+        'Script Generation Complete'
+      );
+      const generationResult: any = await this.scriptGeneratorService.getScriptById(
+        scriptId
+      );
+      this.applyGenerationResult(generationResult);
+      this.isDisabled = false;
+    } catch (err) {
+      // Pretty generic error handling.
+      this.toastr.error(err.message, 'Generic Error');
+      console.error(err);
+      this.isDisabled = false;
+      this.isGenerating = false;
+    }
+  }
+
+  /**
+   * Generated script.
+   */
+  public generationResult: any;
+}
