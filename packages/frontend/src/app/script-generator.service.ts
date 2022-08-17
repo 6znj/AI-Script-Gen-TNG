1c3cf1a16c659cda60bf8dd7de85b990e5c0ebc4
6znj <119874842+6znj@users.noreply.github.com>
2022-08-17 21:10:18 +0000

    remove pyc files

--- packages/frontend/src/app/script-generator.service.ts
+++ packages/frontend/src/app/script-generator.service.ts
@@ -7,4 +7,17 @@ import { environment } from '../environments/environment';
  */
 @Injectable({
   providedIn: 'root'
-}
+})
+export class ScriptGeneratorService {
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Base url of service to use.
+   */
+  public baseUrl = environment.apiBaseUrl;
+
+  /**
+   * Generate a new script.
+   * @param stripePaymentResult Payment information from Stripe.
+   */
+  public gener
