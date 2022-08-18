cbea4657b1183939d9bc9e7dee5e0c291b699620
6znj <119874842+6znj@users.noreply.github.com>
2022-08-18 20:13:52 +0000

    Removed unnecessary dependencies

--- packages/frontend/src/environments/environment.prod.ts
+++ packages/frontend/src/environments/environment.prod.ts
@@ -2,4 +2,9 @@ export const environment = {
   production: true,
   apiBaseUrl: 'http://localhost:3000',
   /**
-   * This i
+   * This is a purely random wait time!
+   * PROD: Replace/remove!
+   */
+  scriptGeneratorWaitTime: 120000,
+  stripeKey: ''
+};
