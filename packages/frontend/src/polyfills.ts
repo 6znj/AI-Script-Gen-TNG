c426360ea4f0beca55f9f95dd22a9057387d3232
6znj <119874842+6znj@users.noreply.github.com>
2022-08-20 06:53:20 +0000

    added methods

--- packages/frontend/src/polyfills.ts
+++ packages/frontend/src/polyfills.ts
@@ -30,4 +30,8 @@
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
- * user can disable parts of macroTask/DomEvents patch
+ * user can disable parts of macroTask/DomEvents patch by setting following flags
+ * because those flags need to be set before `zone.js` being loaded, and webpack
+ * will put import in the top of bundle, so user need to create a separate file
+ * in this directory (for example: zone-flags.ts), and put the following flags
+ * into that file, and then add the following code before importing
