0162712261c26c76938e1a57131c36693380a3ff
6znj <119874842+6znj@users.noreply.github.com>
2022-08-26 11:26:26 +0000

    Folder restructure

--- packages/scriptgen/scriptgen.py
+++ packages/scriptgen/scriptgen.py
@@ -4,4 +4,7 @@ checkpoint_dir = './data/checkpoint'
 sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess, checkpoint_dir=checkpoint_dir)
 
-single_
+single_text = gpt2.generate(sess, return_as_list=True, checkpoint_dir=checkpoint_dir)[0]
+print('-----BEGIN GENERATED TEXT-----')
+print(single_text)
+print('-----END GENERATED TEXT-----')
---
+++ packages/tng_scriptgen_flutter_app/README.md
@@ -0,0 +1,7 @@
+# tng_scriptgen_flutter_app
+
+TNG Script Generator Mobile App
+
+## Getting Started
+
+This project is a starting point for a Flutter applic
