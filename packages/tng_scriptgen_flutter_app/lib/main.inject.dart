fcbdf548b1a53c6b21929ba67fb7148578b52cb0
6znj <119874842+6znj@users.noreply.github.com>
2022-09-18 19:27:24 +0000

    Fix test expectations

---
+++ packages/tng_scriptgen_flutter_app/lib/main.inject.dart
@@ -0,0 +1,5 @@
+import 'main.dart' as _i1;
+import 'services/script_repository.dart' as _i2;
+import 'services/script_generator_api_client.dart' as _i3;
+import 'services/script_sync_service.dart' as _i4;
+import 'dart:asy
