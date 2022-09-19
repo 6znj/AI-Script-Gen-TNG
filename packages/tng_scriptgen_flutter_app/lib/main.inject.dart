73b0c29df8a6558ca028b32d16b90d17980d028b
6znj <119874842+6znj@users.noreply.github.com>
2022-09-19 06:32:41 +0000

    shrug

--- packages/tng_scriptgen_flutter_app/lib/main.inject.dart
+++ packages/tng_scriptgen_flutter_app/lib/main.inject.dart
@@ -2,4 +2,16 @@ import 'main.dart' as _i1;
 import 'services/script_repository.dart' as _i2;
 import 'services/script_generator_api_client.dart' as _i3;
 import 'services/script_sync_service.dart' as _i4;
-import 'dart:asy
+import 'dart:async' as _i5;
+
+class Main$Injector implements _i1.Main {
+  Main$Injector._();
+
+  _i2.ScriptRepository _singletonScriptRepository;
+
+  _i3.ScriptGeneratorApiClient _singletonScriptGeneratorApiClient;
+
+  _i4.ScriptSyncService _singletonScriptSyncService;
+
+  static _i5.Future<_i1.Main> create() async {
+  
