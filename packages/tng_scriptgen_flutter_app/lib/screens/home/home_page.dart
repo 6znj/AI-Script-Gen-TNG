0826bddeaa3eeba9cbaefae9a1c61b17bac03739
6znj <119874842+6znj@users.noreply.github.com>
2022-09-30 12:49:01 +0000

    Travis CI config

--- packages/tng_scriptgen_flutter_app/lib/screens/home/home_page.dart
+++ packages/tng_scriptgen_flutter_app/lib/screens/home/home_page.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:inject/inject.dart';
-import 'package:t
+import 'package:tng_scriptgen_flutter_app/services/script_repository.dart';
+import 'package:tng_scriptgen_flutter_app/services/script_sync_service.dart';
+
+@provide
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Pag
