f2899ef989bff6cfa7d44a6e03724187fcf130b9
6znj <119874842+6znj@users.noreply.github.com>
2022-10-11 20:01:53 +0000

    Update latest release

--- packages/tng_scriptgen_flutter_app/lib/screens/script-list/script_list_page.dart
+++ packages/tng_scriptgen_flutter_app/lib/screens/script-list/script_list_page.dart
@@ -1,1 +1,9 @@
-import 'package:flutter/material.dart
+import 'package:flutter/material.dart';
+import 'package:tng_scriptgen_flutter_app/services/script_repository.dart';
+import 'package:tng_scriptgen_flutter_app/services/script_sync_service.dart';
+
+class ScriptListPage extends StatefulWidget {
+  ScriptRepository _scriptRepository;
+  ScriptSyncService _scriptSyncService;
+
+  ScriptListPage(this._scriptRepository,
