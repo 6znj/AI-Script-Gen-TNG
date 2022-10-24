9d82d1193fe567cfec567e2846f05f61cd50caae
6znj <119874842+6znj@users.noreply.github.com>
2022-10-24 20:50:28 +0000

    use constant

--- packages/tng_scriptgen_flutter_app/lib/services/script_generator_api_client.dart
+++ packages/tng_scriptgen_flutter_app/lib/services/script_generator_api_client.dart
@@ -1,4 +1,15 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'packag
+import 'package:inject/inject.dart';
+
+@provide
+@singleton
+class ScriptGeneratorApiClient {
+  String _token = '';
+  String _baseUrl = 'http://rgtest42-amazon.de';
+
+  Future<Map> login(username, password) async {
+    Map payload = {'username': username, 'password': password};
+    final res = await http.post(this._baseUrl + '/auth/login',
+        h
