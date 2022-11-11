3976e6492fe7c6ce7d3641d1ee503949222ab419
6znj <119874842+6znj@users.noreply.github.com>
2022-11-11 15:51:03 +0000

    Minor addition

--- packages/tng_scriptgen_flutter_app/lib/services/script_sync_service.dart
+++ packages/tng_scriptgen_flutter_app/lib/services/script_sync_service.dart
@@ -9,4 +9,14 @@ class ScriptSyncService {
   final ScriptRepository _repository;
   final ScriptGeneratorApiClient _scriptGeneratorApiClient;
 
-  ScriptSyncService(this._re
+  ScriptSyncService(this._repository, this._scriptGeneratorApiClient);
+
+  sync() async {
+    await _repository.initialize();
+    await _repository.clear();
+
+    final remoteScripts = await _scriptGeneratorApiClient.getScriptsByMe();
+    for (var script in remoteScripts) {
+      script['originalId'] = script['_id'];
+      script['_id'] = null;
+      await _rep
