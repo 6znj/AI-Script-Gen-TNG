ae54529a9c71efbe05c9ac46b10a5af4f07bd900
6znj <119874842+6znj@users.noreply.github.com>
2022-11-11 13:45:50 +0000

    docs: update TOC

--- packages/tng_scriptgen_flutter_app/lib/services/script_sync_service.dart
+++ packages/tng_scriptgen_flutter_app/lib/services/script_sync_service.dart
@@ -1,2 +1,12 @@
 import 'package:inject/inject.dart';
-import 'package:tng_scri
+import 'package:tng_scriptgen_flutter_app/services/script_generator_api_client.dart';
+import 'package:tng_scriptgen_flutter_app/services/script_repository.dart';
+
+@provide
+@singleton
+class ScriptSyncService {
+
+  final ScriptRepository _repository;
+  final ScriptGeneratorApiClient _scriptGeneratorApiClient;
+
+  ScriptSyncService(this._re
